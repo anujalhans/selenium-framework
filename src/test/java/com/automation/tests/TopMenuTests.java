8e20c48fe6576ebf9749d1b6730559c98dc568e1
Anuj Alhans <anuj.alhans01@nagarro.com>
2025-10-14 16:48:34 +0530

    Removed log as it was throwing compilation error

--- src/test/java/com/automation/tests/TopMenuTests.java
+++ src/test/java/com/automation/tests/TopMenuTests.java
@@ -67,8 +67,6 @@ public class TopMenuTests extends BaseTest {
         //Get the title and url
         String title = getDriver().getTitle();
         String url = getDriver().getCurrentUrl();
-        //Log the title......
-        Log.info("Title: " + title);
         Assert.assertTrue(title.contains("Support"),
                 "Expected title to contain the 'Support'. Title: " + title);
     }
